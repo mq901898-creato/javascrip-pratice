c1efd8492349ae8bb276fd3c1b5de0619f9d07bf
Mubashir Qureshi <mq901898@gmail.com>
2025-11-10 13:33:19 +0500

    Fixed submodule issue, added all files

---
+++ practice-4/next.js
@@ -0,0 +1,8 @@
+let num1=prompt("Enter your first Number")
+let num2=prompt("Enter your Secound Number")
+num1=Number(num1);
+num2=Number(num2);
+document.querySelector("#design h2").innerHTML = num1 + num2 ;
+
+
+
